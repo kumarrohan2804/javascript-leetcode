f1451bf38c573868b028095c991bc2c04c9041cc
kumarrohan2804 <kumarrohan2804@gmail.com>
2021-10-21 23:21:11 +0530

    folder location using stack --n

---
+++ Stack/crawlerLog.js
@@ -0,0 +1,20 @@
+var minOperations = function (logs) {
+  let stack = [0];
+  for (let i = 0; i < logs.length; i++) {
+    if (stack.length > 1 && logs[i] == "../") {
+      stack.pop();
+    } else if (logs[i] !== "./" && logs[i] !== "../") {
+      stack.push(1);
+    }
+  }
+  let count = 0;
+  console.log(stack);
+  while (stack.length > 1) {
+    stack.pop();
+    count++;
+  }
+  return count;
+};
+
+let logs = ["./", "../", "./"];
+console.log(minOperations(logs));
